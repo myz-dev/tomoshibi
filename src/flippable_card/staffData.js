b3bf3d21dd9d3ce1a333f9a4f50c97ca5da8d0fa
Mert YIldiz <mert_y52@hotmail.com>
2021-12-19 17:23:01 +0100

    Initial commit

---
+++ src/flippable_card/staffData.js
@@ -0,0 +1,50 @@
+export const staff = [
+    {
+        id: 0,
+          name: "Juanita Doe",
+          position: "CEO & Founder",
+          descr: "Some text that describes me lorem ipsum ipsum lorem.",
+          email: "jane@example.com",
+          img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.photopixels.com%2Fportraits%2Fimages%2FLawyer%2520Headshot%2520on%2520White.jpg&f=1&nofb=1"	
+      },
+    {
+        id: 1,
+          name: "Felipe Martinez",
+          position: "Security",
+          descr: "Some text that describes me lorem ipsum ipsum lorem.",
+          email: "felipe@example.com",
+          img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.photopixels.com%2Fportraits%2Fimages%2FProfessor%2520Headshot.jpg&f=1&nofb=1"	
+      },
+    {
+        id: 2,
+          name: "Shania Jackson",
+          position: "CFO",
+          descr: "Some text that describes me lorem ipsum ipsum lorem.",
+          email: "shania@example.com",
+          img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.photopixels.com%2Fportraits%2Fimages%2FEntrepreneur.jpg&f=1&nofb=1"	
+      },
+    {
+        id: 3,
+          name: "Rusty Blade",
+          position: "Secretary",
+          descr: "Some text that describes me lorem ipsum ipsum lorem.",
+          email: "rblade@example.com",
+          img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.photopixels.com%2Fportraits%2Fimages%2FCreative%2520Ad%2520Agency%25202.jpg&f=1&nofb=1"	
+      },
+    {
+        id: 4,
+          name: "Shelli Honeywell",
+          position: "Consultant",
+          descr: "Some text that describes me lorem ipsum ipsum lorem.",
+          email: "chartreuse@example.com",
+          img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.photopixels.com%2Fportraits%2Fimages%2FFemale%2520Business%2520Headshot%25203.jpg&f=1&nofb=1"	
+      },
+      {
+        id: 5,
+          name: "Strawberry Jo",
+          position: "Lead Agent",
+          descr: "Some text that describes me lorem ipsum ipsum lorem.",
+          email: "strawberry@example.com",
+          img: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.photopixels.com%2Fportraits%2Fimages%2FHeadshot%2520White%2520Background.jpg&f=1&nofb=1"	
+      }
+  ]
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+import FlippableCard from "./../flippable_card/FlippableCard.svelte";
+
+</script>
+
+
+<FlippableCard/>
