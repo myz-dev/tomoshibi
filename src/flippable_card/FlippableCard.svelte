b30ff7f755c1dbfa2483a0c6c76925ad94a52da9
Mert YIldiz <mert_y52@hotmail.com>
2021-12-26 23:04:29 +0100

    Added icons, colors and fixed the audio bug.
    - Audio is now only played after a click on the un-mute button.
    - 4 Pastel colors added.
    - Installed "svelte-icons": "^2.1.0"

--- src/flippable_card/FlippableCard.svelte
+++ src/flippable_card/FlippableCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-import { onMount } from 'svelte';
-
+	import { onMount } from 'svelte';
 	import { Card } from './../Cards';
+	import FaVolumeUp from 'svelte-icons/fa/FaVolumeUp.svelte';
 	export let card: Card = new Card(
 		2,
 		'あ',
@@ -12,16 +12,20 @@ import { onMount } from 'svelte';
 
 	let paused = false;
 	let volume = 0.3;
+	let currentTime = 0;
+	let muted = false;
+	function play_soundfile(): void {
+		paused = false;
+	}
+	onMount(() => {
+		paused = true;
+		muted = false;
+	});
+
+
 </script>
 
-{#if paused}
-	<div class="bg-blue-600">
-		paused is on
-	</div>
-{/if}
-<!-- <button on:click="{() => paused = !paused}"> PLAY! </button>
-<input type="scale" bind:value="{volume}"> -->
-<div on:mouseenter="{() => paused = false}" on:mouseleave="{() => paused = true}" class="mt-4 flex flex-col space-y-5">
+<div class="mt-4 flex flex-col space-y-5">
 	<div>
 		<div class="flip-card w-56 h-56">
 			<div class="flip-card-inner">
@@ -32,13 +36,24 @@ import { onMount } from 'svelte';
 					<p class="antialiased text-6xl font-semibold text-red-400">{card.glyph}</p>
 				</div>
 				<!-- Card Back -->
-				<div class="flip-card-back bg-sky-50 rounded-xl flex items-center justify-center">
+				<div class="flip-card-back bg-sky-50 rounded-xl flex flex-col items-center justify-center">
+					<button
+						on:click={() => {currentTime = 0; paused = false}}
+						class="ml-40 mt-3 h-6 w-6 text-slate-800 hover:shadow-md hover:cursor-pointer hover:text-gray-600"
+					>
+						<FaVolumeUp />
+					</button>
+
 					<figure>
 						<img class="h-36 w-36" src={card.src} alt={card.alt} />
-						<figcaption class="text-3xl">{card.displayed_name}</figcaption>
+						<figcaption class="text-3xl">
+							<span> {card.displayed_name}</span>
+						</figcaption>
 					</figure>
-					<audio preload="auto" bind:paused bind:volume>
-						<source src="test_sound.mp3" type="audio/mp3">
+					<!-- <p class="text-3xl">{card.displayed_name}</p> -->
+					<audio preload="auto" bind:paused bind:volume bind:currentTime bind:muted>
+						<source src="test_sound.mp3" type="audio/mp3" />
+						<p>Your browser does not support audio elements. Consider updating your browser!</p>
 					</audio>
 				</div>
 			</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				pastellake: {
+					800: '#B5EAEA'
+				},
+				pastelbeige: {
+					800: '#EDF6E5'
+				},
+				pastelpink: {
+					800: '#FFBCBC'
+				},pastelred: {
+					800: '#F38BA0'
+				},
+			}
+
+		}
 	},
 
 	plugins: [forms, typography]
